c726e57312f96792d941c23cf1573df20b80577d
idrees04 <idreesar@yahoo.com>
2020-09-08 20:24:08 +0500

    Add project files.

---
+++ OutputCacheFilterMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using OutputCacheFilterMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OutputCacheFilterMVC.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        PersonDBEntities db = new PersonDBEntities();
+        // GET: Home
+        [OutputCache(Duration =10, Location = System.Web.UI.OutputCacheLocation.Client)]
+        //when action mathod call next 10 second DateTime will be save. if we will call after 10 second DateTime will Update
+        public ActionResult Index()
+        {
+            ViewBag.Time = DateTime.Now.ToLongTimeString();
+            return View();
+        }
+
+        [OutputCache(Duration = 30)]
+        public ActionResult GetData()
+        {
+            var data = db.people.ToList();
+            return View(data);
+        }
+    }
+}
